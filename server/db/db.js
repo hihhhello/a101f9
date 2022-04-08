143adab5269b29b1edffdcba0f58d3185dc213af
unknown <anton.hihhhello@gmail.com>
2022-04-08 12:43:05 +0300

    db setup

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    'postgres://postgres:root@localhost:5433/messenger',
+  {
+    logging: false,
+  },
+);
 
 module.exports = db;
