1f861e18fc7e114b8a317fb6d3b1252a269b8442
hihhhello <anton.hihhhello@gmail.com>
2022-04-18 12:40:50 +0300

    feat: changed message realtion to UserConv

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,8 +4,8 @@ const Message = require("./message");
 const UserConversation = require("./user-conversation");
 
 // associations
-Message.belongsTo(Conversation);
-Conversation.hasMany(Message);
+Message.belongsTo(UserConversation);
+UserConversation.hasMany(Message);
 User.belongsToMany(Conversation, { through: UserConversation });
 Conversation.belongsToMany(User, { through: UserConversation });
 
